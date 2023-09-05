a5c89347458ffac307f0d327598c395abf4b134d
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-09-05 00:40:24 -0300

    fix: header and screen page size

--- apps/webclient/app/dashboard/layout.tsx
+++ apps/webclient/app/dashboard/layout.tsx
@@ -7,10 +7,10 @@ export default function DashboardLayout({
 }) {
   return (
     <section className="min-h-screen">
-      <header className="flex justify-between items-center py-6 px-8">
+      <header className="flex justify-between items-center py-6 px-8 w-full absolute">
         <Header />
       </header>
-      <main className="flex h-screen">
+      <main className="flex h-screen pt-28 pb-4">
         <Sidebar />
         {children}
       </main>
